18cac7ce644a0034575a733cf77c09b1ef134e76
Imaginemehere <97543346+Imaginemehere@users.noreply.github.com>
2022-10-07 00:28:02 +0800

    Add files via upload

---
+++ YukkiMusic/utils/inline/suggest.py
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from YukkiMusic import app
+
+
+def suggest_lowkey(_):
+    buttons =[
+    	[
+            InlineKeyboardButton(
+                text=_["S_B_9"],
+                url=f"https://t.me/imagineitsreal",
+            ),
+        ],
+    ]
+
+    return buttons
